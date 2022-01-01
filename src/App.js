0623c5497c835805840199f4695a33095ab4288a
nate-crema <58645920+nate-crema@users.noreply.github.com>
2022-01-01 23:10:22 +0900

    Create Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// components
+import Keyframe from "./components/Keyframe";
+import Display from "./components/Display";
+
+// css
+import "./css/global.css";
+import "./css/components/App.css";
+
+function App({ props }) {
+
+  const [ display_rate, setDisplayRate ] = useState(0.6);
+  const [ keyframe_rate, setKeyframeRate ] = useState(0.4);
+  
+  const [ uiarea_rate, setUIAreaRate ] = useState(0.5);
+  const [ codearea_rate, setCodeAreaRate ] = useState(0.5);
+
+  return <div className="main-wrap"
+    style={{
+      gridTemplateColumns: `${ uiarea_rate * 100 }% ${ codearea_rate * 100 }%`
+    }}
+  >
+    <div className="ui-area" style={{
+      gridTemplateRows: `${ display_rate * 100 }% ${ keyframe_rate * 100 }%`,
+    }}>
+      <Display/>
+      <Keyframe/>
     </div>
-  );
+    <div className="code-area">
+
+    </div>
+  </div>;
 }
 
 export default App;
---
+++ src/components/Keyframe.js
@@ -0,0 +1,9 @@
+import { useState, useEffect, useRef } from 'react';
+
+function Keyframe({ props }) {
+
+
+    return <div></div>;
+}
+
+export default Keyframe;
